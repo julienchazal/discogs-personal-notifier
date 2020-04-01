40ff6f70592e332fa2464ba92517175e19d1b2a0
Julien CHAZAL <julien.chazal@wiseed.com>
2020-04-02 00:13:52 +0200

    replace sapper with snowpack

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Search from './Search';
+    let greet = 'Discogs personal notifier';
+</script>
+
+<div class="wrapper">
+    <h1>{greet}</h1>
+    <Search />
+</div>
+
+
+<style>
+    :global(html) {
+        font-size: 14px;
+        font-family: Arial, sans-serif ;
+    }
+    .wrapper {
+        padding: 3rem 1rem;
+    }
+    h1 {
+        text-align: center;
+        font-size: 2rem;
+        color: #41adff;
+    }
+</style>
---
+++ src/Search.svelte
@@ -0,0 +1,68 @@
+<script>
+    let search = '';
+    let results = [];
+    let q, res;
+
+    async function searchFn() {
+        console.log('q = '+search)
+        q = await fetch('https://api.discogs.com/database/search?q='+search+'&type=release&key=zOXIgBWYIOOXAiDqlfLs&secret=OWkTTrjHpzDTdYABSImEJPAEkubAIqKV');
+        res = await q.json();
+        console.log(res);
+        results = res.results;
+    }
+
+    $: if (search !== '') {
+        searchFn();
+    }
+</script>
+
+<div class="search">
+    <input type="search" bind:value={search} placeholder="Black album, Rien...">
+    <div>
+        <ul>
+            {#each results as r}
+                <li data-id="{r.id}">
+                    <img src="{r.thumb}" alt="">
+                    <span>{r.title}<br>
+                    {#each r.format as f}
+                        <em>{f} /</em>
+                    {/each}
+                    </span>
+                    <span>{r.year}</span>
+                </li>
+            {/each}
+        </ul>
+    </div>
+</div>
+
+<style>
+    .search {
+        text-align: center;
+    }
+    input {
+        font-size: 16px;
+        background: aliceblue;
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        border-bottom: 1px solid #41adff;
+        padding: 1rem;
+        width: 100%;
+    }
+    ul {
+        padding: 0;
+        margin: 0;
+        list-style-type: none;
+    }
+    li {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem 0.5rem;
+    }
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+</style>
