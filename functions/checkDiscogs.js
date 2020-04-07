689733e84c31da88c6561846e5ec1133fd9ebbca
Julien CHAZAL <julien.chazal@wiseed.com>
2020-04-07 17:58:30 +0200

    batch test

--- functions/checkDiscogs.js
+++ functions/checkDiscogs.js
@@ -1,6 +1,21 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Hello, World"
-    });
+// exports.handler = async function(event, context, callback) {
+//     let q = await fetch('https://api.discogs.com/releases/2387793');
+//     let res = await q.json();
+//     console.log(res);
+//     callback(null, {
+//         statusCode: 200,
+//         body: "Hello, World"
+//     });
+// }
+const fetch = require('node-fetch');
+
+exports.handler =  event => {
+    // setInterval(async () => {
+    //     let q = await fetch('https://api.discogs.com/releases/2387793');
+    //     let res = await q.json();
+    //     return {
+    //         statusCode: 200,
+    //         body: JSON.stringify({"en vente" : res.num_for_sale}),
+    //     }
+    // }, 5000)
 }
