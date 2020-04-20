928d95a45056601e0e387d1c6c090f1b717e484e
Julien CHAZAL <julien.chazal@wiseed.com>
2020-04-20 12:48:34 +0200

    WIP

--- functions/discogs.js
+++ functions/discogs.js
@@ -1,37 +1,71 @@
-const fetch = require('node-fetch');
+const https = require('https');
 
 const sendPush = (title, uri, thumb) => {
-    let myHeaders = {"Content-Type": "application/json"}
-            
-    let myInit = { method: 'POST',
-        headers: myHeaders,
-        mode: 'cors',
-        cache: 'default',
-        body: JSON.stringify({
-            "to":"7f184bbe42f6e2ea4fa150",
+    let myHeaders = {
+        "Content-Type": "application/json"
+    }
+
+    const data = JSON.stringify({
+            "to":"b8c4e8a3494ea07d9ba44d",
             "data": {
                 "title": "Discogs personal notifier",
                 "message": title+" en vente ma gueule !!",
                 "url": uri,
                 "image": thumb
             }
-        })
+    });
+            
+    let options = {
+        hostname: 'api.pushy.me',
+        path: '/push?api_key=1078f5606fc2332164c58479f022b786199a838f33d3413b91c6ef53cb6bf1f7',
+        method: 'POST',
+        headers: myHeaders
     };
 
-    fetch('https://api.pushy.me/push?api_key=1078f5606fc2332164c58479f022b786199a838f33d3413b91c6ef53cb6bf1f7',myInit);
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            process.stdout.write(d)
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error)
+    })
+
+    req.write(data)
+    req.end()
 }
 
-const checkDiscogs = async (release, thumb) => {
-    let q = await fetch('https://api.discogs.com/releases/'+release);
-    let res = await q.json();
-    
-    if (res.num_for_sale !== 0) {
-        console.log(res.title);
-        sendPush(res.title, res.uri, thumb);
-    }
+const checkDiscogs = (release, thumb) => {
+
+    let url = 'https://api.discogs.com/releases/'+release;
+
+    https.get(url, {headers: {'User-Agent': 'javascript'}}, (res) => {
+        let body = "";
+
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        res.on("end", () => {
+            try {
+                let json = JSON.parse(body);
+                if (json.num_for_sale !== 0) {
+                    sendPush(json.title, json.uri, thumb);
+                }
+            } catch (error) {
+                console.error('erreur :' + error.message);
+            };
+        });
+
+    }).on("error", (error) => {
+        console.error(error.message);
+    });
 }
 
-// const id = setInterval(() => {
+const id = setInterval(() => {
     // accident num 7
     checkDiscogs('2387793', 'https://img.discogs.com/hnumEOuldwZ2Mi_7Ht6TFNJvG1M=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2387793-1408011015-3502.jpeg.jpg');
 
@@ -40,7 +74,7 @@ const checkDiscogs = async (release, thumb) => {
 
     // Lemonade & brownies
     checkDiscogs('7665741', 'https://img.discogs.com/kXtkKCN0IEpiSRXd5Af47mj_RfU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-7665741-1537723008-5641.jpeg.jpg');
-// }, 1000*60*60*6)
+}, 1000*60*60*6)
 
 
 
